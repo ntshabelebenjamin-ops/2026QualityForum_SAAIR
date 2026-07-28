ae6ead749c9692e7a83d4f11c52f62435f62ff45
ntshabelebenjamin-ops <ntshabelebenjamin@gmail.com>
2026-07-29 00:02:44 +0200

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -1,1 +1,12 @@
-# Reusable charts
+import plotly.express as px
+
+def bar(df, x, y, title):
+
+    fig = px.bar(
+        df,
+        x=x,
+        y=y,
+        title=title
+    )
+
+    return fig
