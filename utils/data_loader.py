107e330b8f31da698945d6968078f6be892dc3c5
ntshabelebenjamin-ops <ntshabelebenjamin@gmail.com>
2026-07-28 23:59:36 +0200

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,1 +1,57 @@
-# Data loading functions
+import pandas as pd
+
+
+def load_kpis():
+
+    return pd.read_csv(
+        "data/institutional/strategic_kpis.csv"
+    )
+
+
+def load_students():
+
+    return pd.read_csv(
+        "data/students/students.csv"
+    )
+
+
+def load_voice():
+
+    return pd.read_csv(
+        "data/students/student_voice.csv"
+    )
+
+
+def load_success():
+
+    return pd.read_csv(
+        "data/students/student_success.csv"
+    )
+
+
+def load_SER():
+
+    return pd.read_csv(
+        "data/academic/self_evaluation_reports.csv"
+    )
+
+
+def load_reviews():
+
+    return pd.read_csv(
+        "data/academic/programme_reviews.csv"
+    )
+
+
+def load_actions():
+
+    return pd.read_csv(
+        "data/operations/action_tracker.csv"
+    )
+
+
+def load_risks():
+
+    return pd.read_csv(
+        "data/institutional/institutional_risk_register.csv"
+    )
